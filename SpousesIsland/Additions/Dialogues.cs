858712d436a3875fbc8b41f1a62dc82f9e3cdac7
Mistyspring <105533011+misty-spring@users.noreply.github.com>
2024-03-17 00:48:38 -0300

    Prepare for 1.6
    Update some of the 1.6 changes to the mod

---
+++ SpousesIsland/Additions/Dialogues.cs
@@ -0,0 +1,42 @@
+﻿using StardewValley;
+
+namespace SpousesIsland.Additions;
+
+internal static class Dialogues
+{
+
+    /// <summary>
+    /// Create dialogue and push to front of stack.
+    /// </summary>
+    /// <param name="who"></param>
+    /// <param name="text"></param>
+    internal static void Push(NPC who, string text)
+    {
+        var dialogue = new Dialogue(who, null, text);
+        who.CurrentDialogue.Push(dialogue);
+    }
+
+    /// <summary>
+    /// Set dialogue 
+    /// </summary>
+    /// <param name="who"></param>
+    /// <param name="text"></param>
+    internal static void Set(NPC who, string text, bool add = true)
+    {
+        var dialogue = new Dialogue(who, null, text);
+        who.setNewDialogue(dialogue, add, false);
+    }
+
+    /// <summary>
+    /// Draws dialogue to screen.
+    /// </summary>
+    /// <param name="who"></param>
+    /// <param name="text"></param>
+    // Because this method doesn't exist anymore in Game1, we do its equiv.
+    public static void Draw(NPC who, string text)
+    {
+        var db = new Dialogue(who, null, text);
+        who.CurrentDialogue.Push(db);
+        Game1.drawDialogue(who);
+    }
+}
---
+++ SpousesIsland/Additions/InstalledMods.cs
@@ -0,0 +1,20 @@
+﻿namespace SpousesIsland.Additions;
+
+internal class InstalledMods
+{
+    public bool StardewExpanded { get; set; } = HasMod("FlashShifter.StardewValleyExpandedCP");
+    public bool LittleNpcs { get; set; } = HasMod("Candidus42.LittleNPCs");
+    public bool Devan { get; set; } = HasMod("mistyspring.DevanNPC");
+    public bool LnhIsland { get; set; } = HasMod("Lnh.IslandOverhaul");
+
+    internal static bool HasMod(string modId)
+    {
+        if (string.IsNullOrWhiteSpace(modId))
+        {
+            ModEntry.Mon.Log("Mod Id can't be empty!");
+            return false;
+        }
+        
+        return ModEntry.Help.ModRegistry.Get(modId) is not null;
+    }
+}
