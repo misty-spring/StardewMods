f44e8e9e1f9e717d395433e206b24e9e5c7f700d
Mistyspring <105533011+misty-spring@users.noreply.github.com>
2024-04-23 17:26:54 +0000

    Create TreeSpawnData.cs
    Initial version, hasn't been tested yet

---
+++ ItemExtensions/Models/TreeSpawnData.cs
@@ -0,0 +1,54 @@
+using StardewModdingAPI;
+using StardewValley;
+
+namespace ItemExtensions.Models;
+
+public class TreeSpawnData
+{
+#if DEBUG
+    private const LogLevel Level = LogLevel.Debug;
+#else
+    private const LogLevel Level =  LogLevel.Trace;
+#endif
+    
+    private static void Log(string msg, LogLevel lv = Level) => ModEntry.Mon.Log(msg, lv);
+    private static IModHelper Helper => ModEntry.Help;
+    
+    //public string TreeId { get; set; }
+    public int GrowthStage { get; set; }
+  
+    //conditional
+    public List<MineSpawn> MineSpawns { get; set; } = new();
+    //conditional
+    internal List<MineSpawn> RealSpawnData { get; set; } = new();
+
+    public bool IsValid()
+    {
+      /*
+        if (string.IsNullOrWhiteSpace(TreeId))
+        {
+            Log("Must specify tree ID! Skipping", LogLevel.Warn);
+            return false;
+        }*/
+
+         foreach (var floorData in MineSpawns)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(floorData.Floors))
+                    continue;
+                
+                floorData.Parse(GetFloors(floorData.Floors)); 
+                RealSpawnData.Add(floorData);
+            }
+            catch (Exception e)
+            {
+                //silent error because it might not happen unless there's no data, i think.
+                Log($"Error: {e}");
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
