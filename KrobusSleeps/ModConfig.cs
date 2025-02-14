0fe75a8cabcd9339845f95bf1f5f12d33f0f241d
Mistyspring <105533011+misty-spring@users.noreply.github.com>
2025-02-14 12:44:49 -0300

    Update 1.1.0

---
+++ KrobusSleeps/ModConfig.cs
@@ -0,0 +1,6 @@
+namespace KrobusSleeps;
+
+internal class ModConfig
+{
+    public bool PreferSingleBed { get; set; } = false;
+}
--- KrobusSleeps/ModEntry.cs
+++ KrobusSleeps/ModEntry.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Locations;
 using StardewValley.Objects;
@@ -9,13 +10,38 @@ namespace KrobusSleeps;
 
 public class ModEntry : Mod
 {
+    private static ModConfig Config { get; set; }
+    
     public override void Entry(IModHelper helper)
     {
+        Config = Helper.ReadConfig<ModConfig>();
+        
+        helper.Events.GameLoop.GameLaunched += OnGameLaunch;
+        
         var harmony = new Harmony(ModManifest.UniqueID);
         Monitor.Log($"Applying Harmony patch \"{nameof(ModEntry)}\": postfixing SDV method \"FarmHouse.getSpouseBedSpot\".");
         harmony.Patch(
             original: AccessTools.Method(typeof(FarmHouse), nameof(FarmHouse.getSpouseBedSpot)),
             postfix: new HarmonyMethod(typeof(ModEntry), nameof(Post_getSpouseBedSpot))
+        );
+    }
+
+    private void OnGameLaunch(object? sender, GameLaunchedEventArgs e)
+    {
+        var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+        
+        // register mod
+        configMenu?.Register(
+            mod: ModManifest,
+            reset: () => Config = new ModConfig(),
+            save: () => Helper.WriteConfig(Config)
+        );
+        
+        configMenu?.AddBoolOption(
+            mod: ModManifest,
+            name: () => Helper.Translation.Get("config.PreferSingleBed.name"),
+            getValue: () => Config.PreferSingleBed,
+            setValue: value => Config.PreferSingleBed = value
         );
     }
 
@@ -26,7 +52,7 @@ public class ModEntry : Mod
 
         //get farmer house's bed
         var farmHouse = Utility.getHomeOfFarmer(Game1.player);
-        var bed = farmHouse.GetBed(BedFurniture.BedType.Double);
+        var bed = farmHouse.GetBed(Config.PreferSingleBed ? BedFurniture.BedType.Single : BedFurniture.BedType.Double);
 
         //if it doesn't exist
         if (bed is null)
